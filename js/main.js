93562b6d775262c3195373e6bdfd746085f4765f
ppopow <pawel.popow@gmail.com>
2020-10-19 22:13:29 +0200

    nowe zadanie37

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function tekst(button) {
+    let result = button;
+
+    if (button (dodaj))  {
+        console.log(dodaj);
+
+    } else if (button (usun)) {
+        console.log(usun);
+    }
+    return result
+    
+}
